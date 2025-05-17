7a283b91cd0b4d39cdce467f335afeb9f90a5eee
IT24102198 <IT24102198@my.sliit.lk>
2025-05-18 04:28:01 +0530

    .model.java - Hold and transfer payment-related information.

--- .idea/.model.java
+++ .idea/.model.java
@@ -1,90 +1,68 @@
-package org.example.service;
+package org.example.model;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.model.Payment;
-import org.springframework.stereotype.Service;
-
-import java.io.File;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.UUID;
 
-@Service
-public class PaymentService {
+public class Payment {
 
-    public static final String PATHNAME = "src/main/resources/payment.json";
+    private String id;
 
-    /**
-     * Get by customer id
-     *
-     * @param id int
-     * @return Payment
-     */
-    public Payment getByCustomerId(int id) {
-        return getPayments().stream().filter(c -> c.getCustomerId() == id).findFirst().orElse(null);
+    private int customerId;
+
+    private String customerName;
+
+    private double amount;
+
+    private int membershipYears;
+
+    private Date paymentDate;
+
+    public Payment() {
+        this.id = UUID.randomUUID().toString();
     }
 
-    /**
-     * Get payments
-     *
-     * @return List of Payment
-     */
-    public List<Payment> getPayments() {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(new File(PATHNAME), new TypeReference<List<Payment>>() {
-            });
-        } catch (Exception e) {
-            return new ArrayList<>();
-        }
+    public String getId() {
+        return id;
     }
 
-    /**
-     * Save new payment
-     *
-     * @param payment Payment
-     */
-    public void saveNewPayment(Payment payment) {
-        try {
-            payment.setPaymentDate(new Date());
-            setAmount(payment);
-            ObjectMapper mapper = new ObjectMapper();
-            List<Payment> payments = getPayments();
-            clearOldPayments(payments, payment.getCustomerId());
-            payments.add(payment);
-            mapper.writeValue(new File(PATHNAME), payments);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public int getCustomerId() {
+        return customerId;
     }
 
-    /**
-     * Clear old payments
-     *
-     * @param payments List Of Payment
-     * @param customerId int
-     */
-    private void clearOldPayments(List<Payment> payments, int customerId) {
-        payments.removeIf(payment -> payment.getCustomerId() == customerId);
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
     }
 
-    /**
-     * Set amount
-     *
-     * @param payment Payment
-     */
-    private void setAmount(Payment payment) {
-        switch (payment.getMembershipYears()) {
-            case 1:
-                payment.setAmount(1000.00);
-                break;
-            case 2:
-                payment.setAmount(2000.00);
-                break;
-            case 3:
-                payment.setAmount(3000.00);
-                break;
-        }
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public int getMembershipYears() {
+        return membershipYears;
+    }
+
+    public void setMembershipYears(int membershipYears) {
+        this.membershipYears = membershipYears;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 }
+
