acdbf9f01c3f57842aad5767a9ced9070626e10b
IT24102198 <IT24102198@my.sliit.lk>
2025-05-18 04:33:36 +0530

    Services.java - Starts your  System using Spring Boot's embedded server

--- .idea/Services.java
+++ .idea/Services.java
@@ -1,7 +1,7 @@
 
 package org.example;
 
-//helps bootstrap and launch the app
+//helps bootstrap and launch the application
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
