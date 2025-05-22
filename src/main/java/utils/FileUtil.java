7655dc1de09addde271facb7aa6a4fc19d199915
IT24102198 <IT24102198@my.sliit.lk>
2025-05-23 04:33:47 +0530

    FileUtil.java

--- src/main/java/utils/FileUtil.java
+++ src/main/java/utils/FileUtil.java
@@ -1,7 +1,8 @@
 package utils;
 
-
+//writing characters to a file
 import java.io.FileWriter;
+//handle input/output related errors
 import java.io.IOException;
 
 public class FileUtil {
@@ -9,8 +10,9 @@ public class FileUtil {
     private static final String FILE_PATH = "C:/Users/user/OneDrive/Desktop/OOP Project/payments.txt";
 
     public static void savePayment(String data) throws IOException {
-        FileWriter writer = new FileWriter(FILE_PATH, true); // append mode
-        writer.write(data);
+
+        //adding data end of the files
+        FileWriter writer = new FileWriter(FILE_PATH, true);
         writer.close();
     }
 }
