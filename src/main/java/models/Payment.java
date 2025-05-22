c84e1ae452dad238faf8733cd975a987f6c64f52
IT24102198 <IT24102198@my.sliit.lk>
2025-05-23 04:33:22 +0530

    Payment.java

--- src/main/java/models/Payment.java
+++ src/main/java/models/Payment.java
@@ -1,4 +1,3 @@
-
 package models;
 
 public class Payment {
@@ -8,6 +7,10 @@ public class Payment {
     private String mode;
     private String date;
 
+    //Default Constructor ( create a Payment object without setting any values)
+    public Payment() {}
+
+    // Parameterized constructor
     public Payment(String name, String memberId, double amount, String mode, String date) {
         this.name = name;
         this.memberId = memberId;
@@ -16,9 +19,49 @@ public class Payment {
         this.date = date;
     }
 
-    public String toFileString() {
-        return name + "," + memberId + "," + amount + "," + mode + "," + date + "\n";
+    // Getters and Setters
+    public String getName() {
+        return name;
     }
 
-    // Getters (optional for further expansion)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    // Updated to use getters for encapsulation
+    public String toFileString() {
+        return getName() + "," + getMemberId() + "," + getAmount() + "," + getMode() + "," + getDate() + "\n";
+    }
 }
